72c3c3ce2e02510e261863d25a2b9950ff12a2f7
Steve Murch <ceo@bigoven.com>
2021-08-29 10:10:22 -0700

    responsive navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+
 const Navbar = () => {
+
+    const [active, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(!active);
+    };
+
     return (
         <nav className="bg-gray-800">
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <button onClick={handleClick} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
 
                             <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -60,7 +69,12 @@ const Navbar = () => {
                 </div>
             </div>
 
-            <div className="sm:hidden" id="mobile-menu">
+            <div
+
+                className={`${active ? '' : 'hidden'
+                    } w-full `}
+
+                id="mobile-menu">
                 <div className="px-2 pt-2 pb-3 space-y-1">
                     <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
 
---
+++ pages/dashboard.js
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div>
+            DASHBOARD PAGE
+        </div>
+    );
+}
+
+export default Dashboard;
